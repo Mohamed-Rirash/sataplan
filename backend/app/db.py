4eb62a380763721df721b1abf3c6a5bab243e811
mohamedrirash <mohamedrirash1245@gmail.com>
2025-03-15 16:36:10 +0300

    connection booling is added

--- backend/app/db.py
+++ backend/app/db.py
@@ -7,7 +7,13 @@ from .config import DATABASE_URL
 Base = declarative_base()
 
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(
+    DATABASE_URL,
+    pool_size=10,  # Max number of connections in pool
+    max_overflow=5,  # Extra connections when pool is full
+    pool_timeout=30,  # Wait time for a connection before raising an error
+    pool_recycle=1800,
+)
 
 LocalSession = scoped_session(
     sessionmaker(autocommit=False, autoflush=False, bind=engine)
