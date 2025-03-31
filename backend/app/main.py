75dd1b8398477ef33a0361b81088d93ba0bd0cf8
mohamedrirash <mohamedrirash1245@gmail.com>
2025-03-31 10:46:58 +0300

    fixing cors bug3

--- backend/app/main.py
+++ backend/app/main.py
@@ -52,10 +52,10 @@ app.add_middleware(
     minimum_size=100,
 )
 
-origins = ["https://sataplaner.vercel.app", "http://localhost:5173"]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=CORS_ALLOW_ORIGINS,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
